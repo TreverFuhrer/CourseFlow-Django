45530e28857e8a7b5ecdaa9fe202e2ab08cce8ee
Aidan Sosnowski <aidansosnowski@gmail.com>
2025-04-14 19:17:09 -0500

    Implemented basic Admin and Enrollment Features
    Still need to implement reports

--- project/administration/admin.py
+++ project/administration/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Course, Enrollment
+
+'''
+Admin Login
+username: admin
+email: admin@gmail.com
+password: 123456
+'''
 
 # Register your models here.
+admin.site.register(Course)
+
+admin.site.register(Enrollment)
--- project/administration/models.py
+++ project/administration/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    seat_limit = models.PositiveIntegerField()
+    prerequisites = models.ManyToManyField('self', blank=True, symmetrical=False)
+    instructor = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL,
+        null=True,
+        related_name='courses',
+    )
+
+    def __str__(self):
+        return self.title
+
+class Enrollment(models.Model):
+    STATUS_OPTIONS = (
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected')
+    )
+    student = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='enrollments'
+    )
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='enrollments')
+    status = models.CharField(max_length=10, choices=STATUS_OPTIONS, default='pending')
+    enrollment_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.student} - {self.course}"
