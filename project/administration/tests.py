0cc839f3b37852089219a2a6d290c7c543defd59
toki <clevertrever31@gmail.com>
2025-04-16 15:38:07 -0500

    added test for adding course prereqs

--- project/administration/tests.py
+++ project/administration/tests.py
@@ -17,3 +17,13 @@ class CourseTests(TestCase):
 
     def test_course_str_returns_title(self):
         self.assertEqual(str(self.course), 'CS361')
+
+    def test_can_add_prereq_to_course(self):
+        prereq = Course.objects.create(
+            title='CS351',
+            description='Data Structures',
+            seat_limit=30,
+            instructor=self.prof
+        )
+        self.course.prerequisites.add(prereq)
+        self.assertIn(prereq, self.course.prerequisites.all())
