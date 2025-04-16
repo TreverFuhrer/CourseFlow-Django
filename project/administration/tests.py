ee2d06bd78dfefbc829834a28b47b97d0e7df365
toki <clevertrever31@gmail.com>
2025-04-16 15:24:43 -0500

    set up basic test structure

--- project/administration/tests.py
+++ project/administration/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class CourseTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.prof = User.objects.create(username='daprof', password='testpass')
